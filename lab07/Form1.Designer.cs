984f589de0ffa0a85a5771a76ee19f5605b21f78
PC832 <PC832@PC832>
2024-10-29 10:37:34 +0700

    Add project files.

---
+++ lab07/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace lab07
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lvCatetory = new System.Windows.Forms.ListView();
+            this.chID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.txtLoai = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 109);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(132, 43);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "lấy danh sách";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // lvCatetory
+            // 
+            this.lvCatetory.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chID,
+            this.chName,
+            this.chType});
+            this.lvCatetory.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lvCatetory.FullRowSelect = true;
+            this.lvCatetory.HideSelection = false;
+            this.lvCatetory.Location = new System.Drawing.Point(0, 166);
+            this.lvCatetory.MultiSelect = false;
+            this.lvCatetory.Name = "lvCatetory";
+            this.lvCatetory.Size = new System.Drawing.Size(800, 284);
+            this.lvCatetory.TabIndex = 1;
+            this.lvCatetory.UseCompatibleStateImageBehavior = false;
+            this.lvCatetory.View = System.Windows.Forms.View.Details;
+            // 
+            // chID
+            // 
+            this.chID.Text = "ID";
+            // 
+            // chName
+            // 
+            this.chName.Text = "Name";
+            // 
+            // chType
+            // 
+            this.chType.Text = "Type";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mã nhóm";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(129, 6);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(226, 20);
+            this.txtID.TabIndex = 3;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(274, 109);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(111, 43);
+            this.btnThem.TabIndex = 4;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tên nhóm thức ăn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Loại";
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(129, 33);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(226, 20);
+            this.txtTen.TabIndex = 7;
+            // 
+            // txtLoai
+            // 
+            this.txtLoai.Location = new System.Drawing.Point(129, 63);
+            this.txtLoai.Name = "txtLoai";
+            this.txtLoai.Size = new System.Drawing.Size(226, 20);
+            this.txtLoai.TabIndex = 8;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(415, 109);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(111, 43);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Cập nhật";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(570, 109);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(111, 43);
+            this.btnXoa.TabIndex = 10;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtLoai);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lvCatetory);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListView lvCatetory;
+        private System.Windows.Forms.ColumnHeader chID;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.TextBox txtLoai;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnXoa;
+    }
+}
+
---
+++ lab07/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace lab07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=PC832;Initial Catalog=Category;Integrated Security=True";
+
+            try
+            {
+                // Sử dụng using để đảm bảo tài nguyên được giải phóng đúng cách
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    sqlConnection.Open(); // Mở kết nối
+                    string query = "SELECT ID, Tên, Loại FROM danhsach";
+                    using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                    {
+                        using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                        {
+                            DisplayCategory(sqlDataReader); // Hiển thị danh mục
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show($"Lỗi cơ sở dữ liệu: {ex.Message}"); // Hiển thị thông báo lỗi
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Có lỗi không mong muốn xảy ra: {ex.Message}"); // Xử lý lỗi không xác định
+            }
+        }
+
+        private void DisplayCategory(SqlDataReader reader)
+        {
+            lvCatetory.Items.Clear(); // Xóa danh sách cũ
+            while (reader.Read())
+            {
+                // Tạo một mục mới cho ListView
+                ListViewItem item = new ListViewItem(reader["ID"].ToString());
+                item.SubItems.Add(reader["Tên"].ToString());
+                item.SubItems.Add(reader["Loại"].ToString());
+                lvCatetory.Items.Add(item); // Thêm mục vào ListView
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=PC832;Initial Catalog=Category;Integrated Security=True";
+
+            // Lấy dữ liệu từ các TextBox
+            string id = txtID.Text; // Giả sử bạn có một TextBox tên là txtID
+            string name = txtTen.Text; // TextBox cho tên
+            string type = txtLoai.Text; // TextBox cho loại
+
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    sqlConnection.Open();
+                    string query = "INSERT INTO danhsach (ID, Tên, Loại) VALUES (@ID, @Tên, @Loại)";
+                    using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                    {
+                        // Thêm tham số vào câu truy vấn
+                        sqlCommand.Parameters.AddWithValue("@ID", id);
+                        sqlCommand.Parameters.AddWithValue("@Tên", name);
+                        sqlCommand.Parameters.AddWithValue("@Loại", type);
+
+                        // Thực thi câu lệnh
+                        int rowsAffected = sqlCommand.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Thêm thành công!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không thể thêm bản ghi.");
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show($"Lỗi cơ sở dữ liệu: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Có lỗi không mong muốn xảy ra: {ex.Message}");
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+                string connectionString = "Data Source=PC832;Initial Catalog=Category;Integrated Security=True";
+                // Lấy ID từ TextBox
+                string idToDelete = txtID.Text; // Giả sử bạn có một TextBox tên là txtID
+                try
+                {
+                    using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                    {
+                        sqlConnection.Open();
+                        string query = "DELETE FROM danhsach WHERE ID = @ID";
+                        using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                        {
+                            // Thêm tham số vào câu truy vấn
+                            sqlCommand.Parameters.AddWithValue("@ID", idToDelete);
+
+                            // Thực thi câu lệnh
+                            int rowsAffected = sqlCommand.ExecuteNonQuery();
+                            if (rowsAffected > 0)
+                            {
+                                MessageBox.Show("Xóa thành công!");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Không tìm thấy bản ghi với ID đã cho.");
+                            }
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"Lỗi cơ sở dữ liệu: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Có lỗi không mong muốn xảy ra: {ex.Message}");
+                }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=PC832;Initial Catalog=Category;Integrated Security=True";
+
+            // Lấy dữ liệu từ các TextBox
+            string idToUpdate = txtID.Text; // ID của bản ghi cần cập nhật
+            string newName = txtTen.Text;   // Tên mới
+            string newType = txtLoai.Text;   // Loại mới
+
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    sqlConnection.Open();
+                    string query = "UPDATE danhsach SET Tên = @Tên, Loại = @Loại WHERE ID = @ID";
+                    using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                    {
+                        // Thêm tham số vào câu truy vấn
+                        sqlCommand.Parameters.AddWithValue("@ID", idToUpdate);
+                        sqlCommand.Parameters.AddWithValue("@Tên", newName);
+                        sqlCommand.Parameters.AddWithValue("@Loại", newType);
+
+                        // Thực thi câu lệnh
+                        int rowsAffected = sqlCommand.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Cập nhật thành công!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không tìm thấy bản ghi với ID đã cho.");
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show($"Lỗi cơ sở dữ liệu: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Có lỗi không mong muốn xảy ra: {ex.Message}");
+            }
+        }
+    }
+}
